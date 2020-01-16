af797b8b1286077961cc9285ac65fdb50370e7d3
supal <supal.iut03@gmail.com>
2020-01-16 17:22:23 +0600

    homework 4

--- src/main/java/com/example/socialservice/resource/UserResource.java
+++ src/main/java/com/example/socialservice/resource/UserResource.java
@@ -5,7 +5,6 @@ import com.example.socialservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController

db0398bf28fc589ee5a1da980e2b3c4064953e66
supal <supal.iut03@gmail.com>
2020-01-16 17:19:49 +0600

    homework 4

--- src/main/java/com/example/socialservice/repository/CommentRepository.java
+++ src/main/java/com/example/socialservice/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.example.socialservice.repository;
 
 import com.example.socialservice.model.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends MongoRepository<Comment, String> {
 }
--- src/main/java/com/example/socialservice/repository/PhotoRepository.java
+++ src/main/java/com/example/socialservice/repository/PhotoRepository.java
@@ -2,6 +2,8 @@ package com.example.socialservice.repository;
 
 import com.example.socialservice.model.Photo;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PhotoRepository extends MongoRepository<Photo, String> {
 }
